f5a8b3fcc3292f1b5f1e1a8faaa5c00d41424e5a
Sujiiiiiii <123052071+Sujiiiiiii@users.noreply.github.com>
2025-06-02 18:42:13 +0900

    add: ply2bin

---
+++ scripts/ply2bin.py
@@ -0,0 +1,14 @@
+import trimesh
+import os
+
+ply_path = "/Users/hironakasuji/Documents/GitHub/M-resarch/assets/models/bunny.ply"
+
+
+def convert_ply_to_binary(ply_path):
+    mesh = trimesh.load(ply_path)
+    out_path = os.path.splitext(ply_path)[0] + "_binary.ply"
+    mesh.export(out_path, file_type='ply')
+    print(f"Converted to binary PLY format at {out_path}")
+
+if __name__ == "__main__":
+    convert_ply_to_binary(ply_path)
