d18b8b5fce5785a562dd39e5c6672e50e7be4367
Sujiiiiiii <123052071+Sujiiiiiii@users.noreply.github.com>
2025-06-02 20:44:32 +0900

    refac

--- src/experiments/bunny.py
+++ src/experiments/bunny.py
@@ -1,4 +1,5 @@
 import mitsuba as mi
+from mitsuba import ScalarTransform4f as T
 
 from utils.utils import save_image
 
@@ -12,7 +13,7 @@ scene = mi.load_dict(
         "sensor": {
             "type": "perspective",
             "fov": 45,
-            "to_world": mi.ScalarTransform4f().look_at(
+            "to_world": T.look_at(
                 origin=[0, 0, 0.5],
                 target=[0, 0, 0],
                 up=[0, 1, 0],
@@ -28,7 +29,7 @@ scene = mi.load_dict(
             "type": "ply",
             "filename": "assets/models/bunny_binary.ply",
             "flip_normals": True,
-            "to_world": mi.ScalarTransform4f().translate([0, -0.1, 0]),
+            "to_world": T.translate([0, -0.1, 0]),
             "bsdf": {
                 "type": "plastic",
                 "diffuse_reflectance": {
